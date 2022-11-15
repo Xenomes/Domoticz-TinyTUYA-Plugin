a2d5a30feebcc56438846b049a56914afa57cadb
Xenomes <xenomes@outlook.com>
2022-11-15 07:12:06 +0100

    Newer version

--- tools/debug_discovery.py
+++ tools/debug_discovery.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 # The script is intended to get a list of all devices available via Tuya API endpoint.
 import tinytuya
+import json
+import os
 
 # TUYA ACCOUNT - Set up a Tuya Account (see PDF Instructions): 
 # https://github.com/jasonacox/tinytuya/files/8145832/Tuya.IoT.API.Setup.pdf
@@ -15,21 +19,32 @@ DEVICEID = "xxxxxxxxxxxxxxxxxxID"
 # NO NEED TO CHANGE ANYTHING BELOW
 
 # Connect to Tuya Cloud
-# c = tinytuya.Cloud()  # uses tinytuya.json 
 c = tinytuya.Cloud(
         apiRegion=REGION, 
         apiKey=APIKEY, 
         apiSecret=APISECRET, 
-        apiDeviceID=DEVICEID)
+        apiDeviceID=DEVICEID
+        )
 
 # Display list of devices
 devices = c.getdevices()
 print("Device List: %r" % devices)
 
-# Display Properties of Device
-result = c.getproperties(DEVICEID)
-print("Properties of device:\n", result)
+if (os.path.exists("dump.json")):
+        f = open("dump.json", "r+")
+else:
+        f = open("dump.json", "w")
+for d in devices:
+        print("Device:\n",d["id"])
 
-# Display Status of Device
-result = c.getstatus(DEVICEID)
-print("Status of device:\n", result)
+        # Display Properties of Device
+        result = c.getproperties(d["id"])
+        print("\nProperties of device:\n", result)
+        f.write("Properties of device:\n" + json.dumps(result, indent=2))
+
+        # Display Status of Device
+        result = c.getstatus(d["id"])
+        print("\nStatus of device:\n", result)
+        f.write("\nStatus of device:\n" + json.dumps(result, indent=2))
+
+f.close()
