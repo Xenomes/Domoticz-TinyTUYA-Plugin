5c0ed921279f6a0d33f360107471fcc04b39c5a3
Xenomes <xenomes@outlook.com>
2022-12-22 20:14:47 +0100

    Fix for incorrect temp_set selection

--- plugin.py
+++ plugin.py
@@ -3,12 +3,12 @@
 # Author: Xenomes (xenomes@outlook.com)
 #
 """
-<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="1.2.2" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
+<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="1.2.3" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
     <description>
         Support forum: <a href="https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441">https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441</a><br/>
         Support forum Dutch: <a href="https://contactkring.nl/phpbb/viewtopic.php?f=60&amp;t=846">https://contactkring.nl/phpbb/viewtopic.php?f=60&amp;t=846</a><br/>
         <br/>
-        <h2>TinyTUYA Plugin v.1.2.2</h2><br/>
+        <h2>TinyTUYA Plugin v.1.2.3</h2><br/>
         The plugin make use of IoT Cloud Platform account for setup up see https://github.com/jasonacox/tinytuya step 3 or see PDF https://github.com/jasonacox/tinytuya/files/8145832/Tuya.IoT.API.Setup.pdf
         <h3>Features</h3>
         <ul style="list-style-type:square">
@@ -630,12 +630,13 @@ def StatusDeviceTuya(Function):
 def SendCommandCloud(ID, CommandName, Status):
     sendfunction = functions[ID]['functions']
     actual_function_name = CommandName
+    CommandName = list([CommandName])
     actual_status = Status
     # Domoticz.Debug("device_functions:" + str(sendfunction))
     # Domoticz.Debug("CommandName:" + str(CommandName))
     # Domoticz.Debug("Status:" + str(Status))
     for item in sendfunction:
-        if CommandName in str(item['code']):
+        if str(CommandName) in str(item['code']):
             actual_function_name = str(item['code'])
     if 'bright_value' in CommandName or 'bright_value_v2' in CommandName:
         actual_status = pct_to_brightness(sendfunction, actual_function_name, Status)
