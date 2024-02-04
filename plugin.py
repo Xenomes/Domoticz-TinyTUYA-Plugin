9819fcfb934f0eb89f27220834d1969ff2700bd3
Xenomes <xenomes@outlook.com>
2024-02-04 21:04:44 +0100

    Fix some issues

--- plugin.py
+++ plugin.py
@@ -145,9 +145,6 @@ class BasePlugin:
                     elif Command == 'On':
                         SendCommandCloud(DeviceID, 'switch', True)
                         UpdateDevice(DeviceID, Unit, 'On', 1, 0)
-                    elif Command == 'Set Level':
-                        SendCommandCloud(DeviceID, 'switch', True)
-                        UpdateDevice(DeviceID, Unit, Level, 1, 0)
                 if not searchCode('switch', function):
                     if Command == 'Off':
                         SendCommandCloud(DeviceID, 'switch_' + str(Unit), False)
@@ -155,18 +152,13 @@ class BasePlugin:
                     elif Command == 'On':
                         SendCommandCloud(DeviceID, 'switch_' + str(Unit), True)
                         UpdateDevice(DeviceID, Unit, 'On', 1, 0)
-                    elif Command == 'Set Level':
-                        SendCommandCloud(DeviceID, 'switch_' + str(Unit), True)
-                        UpdateDevice(DeviceID, Unit, Level, 1, 0)
 
             elif dev_type in ('dimmer'):
                 if Command == 'Off':
                     SendCommandCloud(DeviceID, 'switch_led_' + str(Unit), False)
                     UpdateDevice(DeviceID, Unit, 'Off', 0, 0)
-                elif Command == 'On':
-                    SendCommandCloud(DeviceID, 'switch_led_' + str(Unit), True)
-                    UpdateDevice(DeviceID, Unit, 'On', 1, 0)
                 elif Command == 'Set Level':
+                    SendCommandCloud(DeviceID, 'switch_led_' + str(Unit), True)
                     SendCommandCloud(DeviceID, 'bright_value_' + str(Unit), Level)
                     UpdateDevice(DeviceID, Unit, Level, 1, 0)
 
