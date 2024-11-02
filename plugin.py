78e775daefdd522361b071c223ec9fe140304828
Xenomes <xenomes@outlook.com>
2024-11-02 20:10:56 +0100

    Issue #132 & #133

--- plugin.py
+++ plugin.py
@@ -3,11 +3,11 @@
 # Author: Xenomes (xenomes@outlook.com)
 #
 """
-<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.0.6" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
+<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.0.7" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
     <description>
         Support forum: <a href="https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441">https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441</a><br/>
         <br/>
-        <h2>TinyTUYA Plugin version 2.0.6</h2><br/>
+        <h2>TinyTUYA Plugin version 2.0.7</h2><br/>
         The plugin make use of IoT Cloud Platform account for setup up see https://github.com/jasonacox/tinytuya step 3 or see PDF https://github.com/jasonacox/tinytuya/files/8145832/Tuya.IoT.API.Setup.pdf
         <h3>Features</h3>
         <ul style="list-style-type:square">
@@ -2310,12 +2310,13 @@ def onHandleThread(startup):
 
             #update devices in Domoticz
             if run == 1:
-                Domoticz.Log('Update devices in Domoticz')
-            if bool(online) == False and Devices[dev['id']].TimedOut == 0:
-                UpdateDevice(dev['id'], 1, False, 0, 1)
-            elif online == True and Devices[dev['id']].TimedOut == 1:
-                UpdateDevice(dev['id'], 1, None, 0, 0)
-            elif bool(online) == True and Devices[dev['id']].TimedOut == 0:
+                Domoticz.Log('Updating devices in Domoticz')
+
+            if not bool(online) and Devices[dev['id']].TimedOut == 0:
+                UpdateDevice(dev['id'], nValue=1, sValue="Offline", TimedOut=1)
+            elif bool(online) and Devices[dev['id']].TimedOut == 1:
+                    UpdateDevice(dev['id'], nValue=1, sValue="Online", TimedOut=0)
+            elif bool(online) and Devices[dev['id']].TimedOut == 0:
                 try:
                     def battery_device():
                         # Battery_device
@@ -3832,7 +3833,7 @@ def onHandleThread(startup):
                             currentstatus = StatusDeviceTuya('presence_state')
                             if currentstatus == 'none':
                                 UpdateDevice(dev['id'], 1, False, 0, 0)
-                            elif currentstatus == 'pir':
+                            elif currentstatus != 'none':
                                 UpdateDevice(dev['id'], 1, True, 1, 0)
                         if searchCode('sensitivity', ResultValue):
                             currentmode = StatusDeviceTuya('sensitivity')
