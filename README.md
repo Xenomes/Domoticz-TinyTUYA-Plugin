ded8fe01d578da48b62b31a38b000cbc8e8d5dbb
Xenomes <xenomes@outlook.com>
2026-02-19 20:26:57 +0000

    Add Siren function to Camera #195

--- README.md
+++ README.md
@@ -100,6 +100,7 @@ I had only a RGBWW light to fully test the script, if there is a fuction missing
 | 2.3.6 | Add Cover detection #192 |
 | 2.3.7 | Modification for Sensor detected as Switch #193 |
 | 2.3.8 | Add EPT ultrasonic sensor 3m #194 |
+| 2.3.9 | Add Siren function to Camera #195 |
 
  [The full Change log](CHANGELOG.md)
 
--- plugin.py
+++ plugin.py
@@ -3,11 +3,11 @@
 # Author: Xenomes (xenomes@outlook.com)
 #
 """
-<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.3.8" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
+<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.3.9" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
     <description>
         Support forum: <a href="https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441">https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441</a><br/>
         <br/>
-        <h2>TinyTUYA Plugin version 2.3.8</h2><br/>
+        <h2>TinyTUYA Plugin version 2.3.9</h2><br/>
         The plugin make use of IoT Cloud Platform account for setup up see https://github.com/jasonacox/tinytuya step 3 or see PDF https://github.com/jasonacox/tinytuya/files/8145832/Tuya.IoT.API.Setup.pdf
         <h3>Features</h3>
         <ul style="list-style-type:square">
@@ -490,6 +490,12 @@ class BasePlugin:
                         UpdateDevice(DeviceID, Unit, False, 0, 0)
                     elif Command == 'On' and Unit == 2:
                         SendCommandCloud(DeviceID, 'floodlight_switch', True)
+                        UpdateDevice(DeviceID, Unit, True, 1, 0)
+                    if Command == 'Off' and Unit == 5:
+                        SendCommandCloud(DeviceID, 'decibel_switch', False)
+                        UpdateDevice(DeviceID, Unit, False, 0, 0)
+                    elif Command == 'On' and Unit == 5:
+                        SendCommandCloud(DeviceID, 'decibel_switch', True)
                         UpdateDevice(DeviceID, Unit, True, 1, 0)
 
                 if dev_type == 'fan':
@@ -1915,6 +1921,8 @@ def onHandleThread(startup):
                         Domoticz.Unit(Name=dev['name'] + ' (Motion switch)', DeviceID=dev['id'], Unit=3, Type=244, Subtype=73, Switchtype=0, Image=9, Used=1).Create()
                     if createDevice(dev['id'], 4) and searchCode('basic_indicator', StatusProperties):
                         Domoticz.Unit(Name=dev['name'] + ' (Indicator)', DeviceID=dev['id'], Unit=4, Type=244, Subtype=73, Switchtype=0, Image=9, Used=1).Create()
+                    if createDevice(dev['id'], 5) and searchCode('decibel_switch', StatusProperties):
+                        Domoticz.Unit(Name=dev['name'] + ' (Siren)', DeviceID=dev['id'], Unit=5, Type=244, Subtype=73, Switchtype=0, Image=9, Used=1).Create()
 
                 if dev_type == 'fan':
                     if createDevice(dev['id'], 1) and searchCode('switch', FunctionProperties):
@@ -3728,11 +3736,6 @@ def onHandleThread(startup):
                             currentstatus = StatusDeviceTuya('liquid_depth')
                             UpdateDevice(dev['id'], 72, currentstatus * 100, 0, 0)
 
-
-
-
-
-
                         # if searchCode('alarm_switch', ResultValue):
                         #     currentstatus = StatusDeviceTuya('alarm_switch')
                         #     UpdateDevice(dev['id'], 47, bool(currentstatus), int(bool(currentstatus)), 0)
@@ -3766,6 +3769,9 @@ def onHandleThread(startup):
                         if searchCode('basic_indicator', StatusProperties):
                             currentstatus = StatusDeviceTuya('basic_indicator')
                             UpdateDevice(dev['id'], 4, bool(currentstatus), int(bool(currentstatus)), 0)
+                        if searchCode('decibel_switch', StatusProperties):
+                            currentstatus = StatusDeviceTuya('decibel_switch')
+                            UpdateDevice(dev['id'], 5, bool(currentstatus), int(bool(currentstatus)), 0)
 
                     if dev_type == 'fan':
                         if searchCode('switch', FunctionProperties):
@@ -4749,13 +4755,12 @@ def DumpConfigToLog():
 def DeviceType(category, product_id=None):
     'convert category to device type'
     'https://github.com/tuya/tuya-home-assistant/wiki/Supported-Device-Category'
-    # if product_id in {'uoa3mayicscacseb', 'igtakqsfhbr7qsp7', 'vmyibm9bvdbudprp', 'x3o8epevyeo3z3oa'}:
-    #     result = 'cover'
-    #elif
-    if product_id == 'chfpey4klfcp1ipl':
+    if product_id in {'uoa3mayicscacseb', 'igtakqsfhbr7qsp7', 'vmyibm9bvdbudprp', 'x3o8epevyeo3z3oa'}:
+        result = 'cover'
+    elif product_id == 'chfpey4klfcp1ipl':
         result = 'dimmer'
-    elif product_id in {'x3o8epevyeo3z3oa', 'gk0d4i8g5akryd9d'}:
-        result = 'sensor'
+    # elif product_id in {'x3o8epevyeo3z3oa', 'gk0d4i8g5akryd9d'}:
+    #     result = 'sensor'
     elif product_id == 'p6sqiuesvhmhvv4f':
         result = 'doorcontact'
     elif category in {'kg', 'cz', 'pc', 'tdq', 'znjdq', 'szjqr', 'aqcz'}:
