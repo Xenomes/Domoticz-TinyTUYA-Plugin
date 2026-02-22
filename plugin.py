537769faa112af2df0732e85b70e312b0d828b4b
Xenomes <xenomes@outlook.com>
2026-02-22 09:19:17 +0000

    Update Doorbell

--- plugin.py
+++ plugin.py
@@ -3,11 +3,11 @@
 # Author: Xenomes (xenomes@outlook.com)
 #
 """
-<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.3.9B" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
+<plugin key="tinytuya" name="TinyTUYA (Cloud)" author="Xenomes" version="2.3.9C" wikilink="" externallink="https://github.com/Xenomes/Domoticz-TinyTUYA-Plugin.git">
     <description>
         Support forum: <a href="https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441">https://www.domoticz.com/forum/viewtopic.php?f=65&amp;t=39441</a><br/>
         <br/>
-        <h2>TinyTUYA Plugin version 2.3.9B</h2><br/>
+        <h2>TinyTUYA Plugin version 2.3.9C</h2><br/>
         The plugin make use of IoT Cloud Platform account for setup up see https://github.com/jasonacox/tinytuya step 3 or see PDF https://github.com/jasonacox/tinytuya/files/8145832/Tuya.IoT.API.Setup.pdf
         <h3>Features</h3>
         <ul style="list-style-type:square">
@@ -543,7 +543,12 @@ class BasePlugin:
                     elif Command == 'On' and Unit == 11:
                         SendCommandCloud(DeviceID, 'floodlight_switch', True)
                         UpdateDevice(DeviceID, Unit, True, 1, 0)
-
+                    if Command == 'Set Level' and Unit == 12:
+                        SendCommandCloud(DeviceID, 'ipc_siren_volume', Level)
+                        UpdateDevice(DeviceID, Unit, Level, 1, 0)
+                    if Command == 'Set Level' and Unit == 13:
+                        SendCommandCloud(DeviceID, 'ipc_siren_duration', Level)
+                        UpdateDevice(DeviceID, Unit, Level, 1, 0)
                 if dev_type == 'fan':
                     if Command == 'Off' and Unit == 1:
                         SendCommandCloud(DeviceID, 'switch', False)
@@ -1991,9 +1996,31 @@ def onHandleThread(startup):
                                 options['LevelActions'] = ''
                                 options['LevelNames'] = '|'.join(mode)
                                 options['SelectorStyle'] = '0' if len(mode) < 5 else '1'
-                        Domoticz.Unit(Name=dev['name'] + ' (Night vision mode)', DeviceID=dev['id'], Unit=10, Type=244, Subtype=62, Switchtype=18, Options=options, Used=1).Create()
+                        Domoticz.Unit(Name=dev['name'] + ' (Night vision mode)', DeviceID=dev['id'], Unit=10, Type=244, Subtype=62, Switchtype=18, Options=options, Image=9, Used=1).Create()
                     if createDevice(dev['id'], 11) and searchCode('floodlight_switch', StatusProperties):
                         Domoticz.Unit(Name=dev['name'] + ' (Floodlight)', DeviceID=dev['id'], Unit=11, Type=244, Subtype=73, Switchtype=0, Image=9, Used=1).Create()
+                    if createDevice(dev['id'], 12) and searchCode('ipc_siren_volume', FunctionProperties):
+                        for item in FunctionProperties:
+                            temp = 'ipc_siren_volume'
+                            if item['code'] == temp:
+                                the_values = json.loads(item['values'])
+                                options = {}
+                                options['ValueStep'] = get_scale(StatusProperties, temp, the_values.get('step'))
+                                options['ValueMin'] = get_scale(StatusProperties, temp, the_values.get('min'))
+                                options['ValueMax'] = get_scale(StatusProperties, temp, the_values.get('max'))
+                                options['ValueUnit'] = the_values.get('unit')
+                        Domoticz.Unit(Name=dev['name'] + ' (Siren Volume)', DeviceID=dev['id'], Unit=12, Type=242, Subtype=1, Options=options, Image=8, Used=1).Create()
+                    if createDevice(dev['id'], 13) and searchCode('ipc_siren_duration', FunctionProperties):
+                        for item in FunctionProperties:
+                            temp = 'ipc_siren_duration'
+                            if item['code'] == temp:
+                                the_values = json.loads(item['values'])
+                                options = {}
+                                options['ValueStep'] = get_scale(StatusProperties, temp, the_values.get('step'))
+                                options['ValueMin'] = get_scale(StatusProperties, temp, the_values.get('min'))
+                                options['ValueMax'] = get_scale(StatusProperties, temp, the_values.get('max'))
+                                options['ValueUnit'] = the_values.get('unit')
+                        Domoticz.Unit(Name=dev['name'] + ' (Siren Duration)', DeviceID=dev['id'], Unit=13, Type=242, Subtype=1, Options=options, Image=21, Used=1).Create()
 
                 if dev_type == 'fan':
                     if createDevice(dev['id'], 1) and searchCode('switch', FunctionProperties):
@@ -3866,6 +3893,14 @@ def onHandleThread(startup):
                         if searchCode('loodlight_switch', StatusProperties):
                             currentstatus = StatusDeviceTuya('floodlight_switch')
                             UpdateDevice(dev['id'], 11, bool(currentstatus), int(bool(currentstatus)), 0)
+                        if  searchCode('ipc_siren_volume', StatusProperties):
+                            currentstatus = StatusDeviceTuya('ipc_siren_volume')
+                            if str(currentstatus) != str(Devices[dev['id']].Units[12].nValue):
+                                UpdateDevice(dev['id'], 12, str(currentstatus), 0, 0)
+                        if  searchCode('ipc_siren_duration', StatusProperties):
+                            currentstatus = StatusDeviceTuya('ipc_siren_duration')
+                            if str(currentstatus) != str(Devices[dev['id']].Units[13].nValue):
+                                UpdateDevice(dev['id'], 13, str(currentstatus), 0, 0)
 
                     if dev_type == 'fan':
                         if searchCode('switch', FunctionProperties):
